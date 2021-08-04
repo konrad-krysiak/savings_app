668f41f1e7011452ad9b67bf74851a8bb836dbf6
KlaraKot <klara.tytus.kot@gmail.com>
2021-08-04 14:22:28 +0200

    dodanie piggyBankSchema

--- server/db/models/savings_app_db.js
+++ server/db/models/savings_app_db.js
@@ -16,18 +16,32 @@ const accountSchema = mongoose.Schema({
 
 //wpłata
 const paymentSchema = mongoose.Schema({
+  idPayment: {type: Int16Array, required: true},
   paymantCash: { type: Int16Array, required: false },
   paymentAccount: { type: Int16Array, required: false },
+  date: {type: date, required: true}
 });
 
 //wypłata
 const paycheckmentSchema = mongoose.Schema({
+  idPaycheck: {type: Int16Array, required: true},
   paycheckmantCash: { type: Int16Array, required: false },
   paycheckmentAccount: { type: Int16Array, required: false },
+  date: {type: date, required: true}
 });
+
+const piggyBankSchema = mongoose.Schema({
+  idPiggy: {type: Int16Array, required: true},
+  namePiggy: {type: String, required: false},
+  description: {type: String, required: false},
+  amount: {type: Int16Array, required: false},
+  dateOfEnd: {type: date, required: false},
+  dateOfBegin: {type: date, required: true}
+})
 
 export default mongoose.model('User', userSchema);
 export default mongoose.model('Account', accountSchema);
 export default mongoose.model('Payment', paymentSchema);
 export default mongoose.model('Paycheck', paycheckSchema);
+export default mongoose.model('PiggyBank', piggyBankSchema);
 
